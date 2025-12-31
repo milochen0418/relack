643e2d950b13797e97dcfabb5fbaac6a860f5aca
milochen0418 <milo_chen@mail2000.com.tw>
2025-12-31 20:10:51 +0800

    google login integration in local project

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,12 @@
 import reflex as rx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config = rx.Config(
     app_name="relack",
+    #google_client_id=os.environ.get("GOOGLE_CLIENT_ID"),
     plugins=[
         rx.plugins.TailwindV3Plugin(),
         rx.plugins.sitemap.SitemapPlugin(),
