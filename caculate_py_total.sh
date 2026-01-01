e24c2f6566e36359d46bc3e299fa405f5b68ee9b
milochen0418 <milo_chen@mail2000.com.tw>
2026-01-01 16:54:26 +0800

    add script to figure how many py code I use in this project

---
+++ caculate_py_total.sh
@@ -0,0 +1,5 @@
+# Calculate the total number of lines of Python code in the current directory,
+echo "Calculating total lines of Python code..."
+# excluding files in the 'testcases' directory, '__pycache__' directories, and
+# the '.venv' directory.
+find . -name "*.py" -not -path "./testcases/*" -not -path "*/__pycache__/*" -not -path "./.venv/*" | xargs wc -l
