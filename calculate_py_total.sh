29ead1754ec5edea7336faef0a4f3c9075c51b69
milochen0418 <milo_chen@mail2000.com.tw>
2026-01-12 05:12:28 +0800

    change name

--- calculate_py_total.sh
+++ calculate_py_total.sh
@@ -2,4 +2,9 @@
 echo "Calculating total lines of Python code..."
 # excluding files in the 'testcases' directory, '__pycache__' directories, and
 # the '.venv' directory.
-find . -name "*.py" -not -path "./testcases/*" -not -path "*/__pycache__/*" -not -path "./.venv/*" | xargs wc -l
+find . -name "*.py" \
+	-not -path "./testcases/*" \
+	-not -path "*/__pycache__/*" \
+	-not -path "./.venv/*" \
+	-not -path "./.web/*" \
+	| xargs wc -l
